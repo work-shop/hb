5e86d848bae70b4f06aa89e59a38f054cb4dbff0
Nic Schumann <nicolas_schumann@brown.edu>
2014-06-10 15:09:30 -0400

    minor

--- wp-content/themes/hb/projects/projects.php
+++ wp-content/themes/hb/projects/projects.php
@@ -10,18 +10,37 @@
 
 <div id="body" class="single project">
 
-
-
 	<?php 
 		$query = new WP_Query(
 			array(
+				"post_type" => "projects",
+				"post_status" => "publish",
 				"posts_per_page" => -1
 			)
 		);
+
+		$grid_accumulator = '<div id="project-grid" class="row">';
+		$list_accumulator = '<div id="project-list">';
+
+		while ( $query->have_posts() ) : $query->the_post();
+
+		$grid_accumulator .= '<div class="grid-element" class="col-sm-2">';
+						  .  ''
+
+
+
 	?>
 
 
-	<?php wp_reset_postdata(); ?>
+
+
+
+	<?php 
+
+		endwhile;
+		wp_reset_postdata(); 
+
+	?>
 
 
 </div>
