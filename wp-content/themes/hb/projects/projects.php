a42f59cab164586d95c94d7866f9a57d76561904
Nic Schumann <nicolas_schumann@brown.edu>
2014-06-10 16:52:09 -0400

    Built the archive-projects page

--- wp-content/themes/hb/projects/projects.php
+++ wp-content/themes/hb/projects/projects.php
@@ -13,7 +13,7 @@
 	<?php 
 		$query = new WP_Query(
 			array(
-				"post_type" => "projects",
+				"post_type" => "project",
 				"post_status" => "publish",
 				"posts_per_page" => -1
 			)
@@ -22,23 +22,38 @@
 		$grid_accumulator = '<div id="project-grid" class="row">';
 		$list_accumulator = '<div id="project-list">';
 
-		while ( $query->have_posts() ) : $query->the_post();
+		while ( $query->have_posts() ) {
 
-		$grid_accumulator .= '<div class="grid-element" class="col-sm-2">';
-						  .  ''
+			$query->the_post();
 
+			$grid_accumulator .= '<div class="grid-element col-sm-2">'
+							  .  '<a href="'.get_permalink().'">';
 
+			$list_accumulator .= '<div class="list-element" class="row">'
+							  .  '<a href="'.get_permalink().'">';
 
-	?>
+			if ( has_post_thumbnail() ) {
+				$grid_accumulator .= get_the_post_thumbnail( );
+				$list_accumulator .= '<div class="col-sm-4">'.get_the_post_thumbnail( ).'</div>';
+			}
+			
+			$list_accumulator .= '<div class="col-sm-8">'
+							  .  '<h3>'.get_the_title().'</h3>'
+							  .  '<p>'.get_field( "description", get_the_ID() ).'</p>'
+							  .  '</div>';
 
+			$grid_accumulator .= '</a></div>';
+			$list_accumulator .= '</a></div>';
 
+		}
 
+		$grid_accumulator .= "</div>";
+		$list_accumulator .= "</div>";
 
-
-	<?php 
-
-		endwhile;
 		wp_reset_postdata(); 
+
+		echo $grid_accumulator;
+		echo $list_accumulator;
 
 	?>
 
