e7b4a7da8864f81fea8efe36c67d910060ce88ed
Greg Nemes <mail@gregnemes.com>
2014-06-18 15:16:57 -0400

    Changes before upload to dev site

--- wp-content/themes/hb/includes/single-content-nav.php
+++ wp-content/themes/hb/includes/single-content-nav.php
@@ -15,6 +15,9 @@
 						<h4><?php the_title(); ?></h4>
 						<ul>
 							<li><a href="#" data-target=".gallery" class="on toggle gallery-toggle"><span class="icon " data-icon="&#8225;"></span></a></li>
+							<?php if ( $video = get_field('video') ) : ?>
+								<li><a href="#" data-target=".video" class="off toggle video-toggle"><span class="icon " data-icon="P"></span></a></li>	
+							<?php endif; ?>						
 							<li><a href="#" data-target=".list" class="off toggle list-toggle"><span class="icon " data-icon="&Acirc;"></span></a></li>
 							<li><a href="#" data-target=".grid" class="off toggle grid-toggle"><span class="icon " data-icon="\"></span></a></li>									
 							<li><a href="#" data-target=".info" class="off toggle info-toggle"><span class="icon " data-icon="="></span></a></li>														
